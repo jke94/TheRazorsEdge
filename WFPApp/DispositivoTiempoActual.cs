94cfa0be258384527ad906978e8b4e131456d7cc
jke94 <jcarracedo.dev@gmail.om>
2020-12-19 15:31:55 +0100

    Zero infraestructure.

---
+++ WFPApp/DispositivoTiempoActual.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFPApp
+{
+    public class DispositivoTiempoActual
+    {
+        // otras métodos y propiedades del dispositivo
+
+        public void ActualizarPantallaDipositivo(object sender, Tuple<decimal, decimal, decimal> medidas)
+        {
+            var temperatura = medidas.Item1;
+            var presion = medidas.Item2;
+            var humedad = medidas.Item3;
+
+            // cosas importantes que hacer con las medidas
+        }
+    }
+}
---
+++ WFPApp/EstacionMetereologica.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WFPApp
+{
+    public class EstacionMetereologica
+    {
+        public decimal Temperatura { get; private set; }
+        public decimal Presion { get; private set; }
+        public decimal Humedad { get; private set; }
+
+        public string TextAux = "30 ºC";
+
+        public event EventHandler<Tuple<decimal, decimal, decimal>> HaCambiadoElTiempo;
+
+        public void AumentarLaTemperaturaEnGrados(int grados)
+        {
+            Temperatura = grados + 1;
+
+            Notificar();
+        }
+
+        public void Notificar()
+        {
+            var medidas = new Tuple<decimal, decimal, decimal>(Temperatura, Humedad, Presion);
+
+            if (HaCambiadoElTiempo != null)
+                HaCambiadoElTiempo.Invoke(this, medidas);
+        }
+    }
+}
