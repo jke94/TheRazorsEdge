9fcd086f37da78757f9a95c124bc4ec20b009817
jke94 <jcarracedo.dev@gmail.om>
2021-01-06 14:02:55 +0100

    Tests in EstacionMetereologica class

---
+++ 02.TestingApplication/TestsServerWPFApplication/Core/TestsEstacionMetereologica.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using ServerWPFApplication.Core;
+
+namespace TestsServerWPFApplication.Core
+{
+    [TestFixture]
+    class TestsEstacionMetereologica
+    {
+        private EstacionMetereologica estacionMetereologica;
+
+        [SetUp]
+        public void Init()
+        {
+            estacionMetereologica = new EstacionMetereologica();
+        }
+
+        [Test(Description = "Test to check that constructor returns an object correctly built.")]
+        public void TestConstructorAreNotNull()
+        {
+            Assert.AreNotEqual(null, estacionMetereologica);
+        }
+    }
+}
